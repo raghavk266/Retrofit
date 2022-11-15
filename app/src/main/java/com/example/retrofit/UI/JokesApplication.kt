0d2521f626a12a53cf7b2d847b8b6b54ea518135
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-11-16 00:26:22 +0530

    repo instance from application

--- app/src/main/java/com/example/retrofit/UI/JokesApplication.kt
+++ app/src/main/java/com/example/retrofit/UI/JokesApplication.kt
@@ -1,10 +1,16 @@
 package com.example.retrofit.UI
 
 import android.app.Application
+import android.util.Log
+import com.example.retrofit.data.Retrofits
 import com.example.retrofit.data.repo.JokesRepository
 
+private const val TAG = "JokesApplication"
 class JokesApplication : Application() {
+    lateinit var repo : JokesRepository
     override fun onCreate() {
         super.onCreate()
+        Log.e(TAG,"inAppOnCreate")
+         repo = JokesRepository(Retrofits.apiInstance)
     }
 }
--- app/src/main/java/com/example/retrofit/UI/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/UI/MainActivity.kt
@@ -3,23 +3,31 @@ package com.example.retrofit.UI
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.retrofit.R
 import com.example.retrofit.data.Retrofits
 import com.example.retrofit.data.datasource.JokesApi
 import com.example.retrofit.data.repo.JokesRepository
+import com.example.retrofit.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    lateinit private var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val api = Retrofits.apiInstance
-        val repo = JokesRepository(api)
+        Log.e(TAG,"onCreate")
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+//        val api = Retrofits.apiInstance
+//        val repo = JokesRepository(api)
+        val repo = (this.application as JokesApplication).repo
         val viewModel = ViewModelProvider(this,MyViewModelFactory(repo)).get(JokesViewModel::class.java)
-        viewModel.getRandomJoke()
+        mBinding.makeRequestButton.setOnClickListener{
+            viewModel.getRandomJoke()
+        }
         viewModel._joke.observe(this){
             Log.e(TAG,it.toString())
         }
+
     }
 }
