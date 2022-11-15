fcae43ce9f92e4e484c36e2f084f41c7d28ca65d
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-11-15 23:01:47 +0530

    establish a constants required for the request and the model

---
+++ app/src/main/java/com/example/retrofit/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.retrofit
+
+val headerXRapidMap = hashMapOf<String,String>(Pair("X-RapidAPI-Key","b75736a949msh2cbebe099219278p19dc4ejsn7596875ceae1"))
+val headerXRapidHostMap = hashMapOf<String,String>(Pair("X-RapidAPI-Host","matchilling-chuck-norris-jokes-v1.p.rapidapi.com"))
+val headerAccept = hashMapOf(Pair("accept","application/json"))
+val headerUseQuery = hashMapOf(Pair("useQueryString","true"))
+const val BASE_URL = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/"
---
+++ app/src/main/java/com/example/retrofit/data/model/CNJokes.kt
@@ -0,0 +1,11 @@
+package com.example.retrofit.data.model
+
+data class CNJokes(
+    val categories: List<Any>,
+    val created_at: String,
+    val icon_url: String,
+    val id: String,
+    val updated_at: String,
+    val url: String,
+    val value: String
+)
