756176257df29a3e757e426c83b257f02a2088e7
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-11-16 17:52:04 +0530

    change the status bar color

--- app/src/main/java/com/example/retrofit/UI/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/UI/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.retrofit.UI
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
-import androidx.lifecycle.ViewModelProvider
 import com.example.retrofit.R
-import com.example.retrofit.data.Retrofits
-import com.example.retrofit.data.datasource.JokesApi
-import com.example.retrofit.data.repo.JokesRepository
 import com.example.retrofit.databinding.ActivityMainBinding
+
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
@@ -18,7 +18,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.e(TAG,"onCreate")
-        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, com.example.retrofit.R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window: Window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.setStatusBarColor(this.resources.getColor(android.R.color.transparent))
+        }
 //        val api = Retrofits.apiInstance
 //        val repo = JokesRepository(api)
         supportFragmentManager.commit {
